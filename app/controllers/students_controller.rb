5087cc65e434ff6c2e0bcefb1da8c6167a17540e
BILALZAHEER <bitf15m511@pucit.edu.pk>
2019-07-18 18:03:06 +0500

    Navigation menu update with bootstrap

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,31 +1,17 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :edit, :update, :destroy]
-
-  # GET /students
-  # GET /students.json
   def index
     @students = Student.all
   end
-
-  # GET /students/1
-  # GET /students/1.json
   def show
   end
-
-  # GET /students/new
   def new
     @student = Student.new
   end
-
-  # GET /students/1/edit
   def edit
   end
-
-  # POST /students
-  # POST /students.json
   def create
     @student = Student.new(student_params)
-
     respond_to do |format|
       if @student.save
         format.html { redirect_to @student, notice: 'Student was successfully created.' }
@@ -36,10 +22,7 @@ class StudentsController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /students/1
-  # PATCH/PUT /students/1.json
-  def update
+def update
     respond_to do |format|
       if @student.update(student_params)
         format.html { redirect_to @student, notice: 'Student was successfully updated.' }
@@ -50,9 +33,6 @@ class StudentsController < ApplicationController
       end
     end
   end
-
-  # DELETE /students/1
-  # DELETE /students/1.json
   def destroy
     @student.destroy
     respond_to do |format|
@@ -60,14 +40,10 @@ class StudentsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_student
       @student = Student.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
       params.require(:student).permit(:name, :age, :rollno, :email)
     end
